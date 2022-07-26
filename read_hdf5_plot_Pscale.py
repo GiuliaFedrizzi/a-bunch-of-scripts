615a643cfa866af0b6da7295e083b46d1639744d
GiuliaFedrizzi <scgf@leed.ac.uk>
2022-07-26 23:47:05 +0100

    plot multiple lines

--- read_hdf5_plot_Pscale.py
+++ read_hdf5_plot_Pscale.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 # run from gaussTime/
 h5_200 = "my_h5.h5"
@@ -10,10 +11,30 @@ h5_050 = "../gaussScale/scale050/my_h5.h5"
 store200 = pd.HDFStore(h5_200)
 store050 = pd.HDFStore(h5_050)
 
+# for my_key in store200.keys():
+#     df = store200[my_key]
+    #ax1 = sns.lineplot(data=df_sigma_1_0_gaussTime02_t1e3_scale200,x='x_coordinate',y='t_20000')
 # /sigma_1_0/gaussTime02/t1e3
-df_sigma_1_0_gaussTime02_t1e3_scale050 = store200["/sigma_1_0/gaussTime02/t1e3"]
-df_sigma_1_0_gaussTime02_t1e3_scale200 = store050['/sigma_1_0/gaussTime02/t1e3']
-for col_name in df_sigma_1_0_gaussTime02_t1e3_scale200.columns:
-    print(col_name)
-sns.lineplot(data=df_sigma_1_0_gaussTime02_t1e3_scale200,x='x_coordinate',y='t_20000')
+my_keys = store200.keys()
+df = store200[my_keys[0]]
+df1 = store050[my_keys[0]]
+
+# convert to long (tidy) form
+dfm = df.melt('x_coordinate', var_name='time', value_name='vals')
+dfm1 = df.melt('x_coordinate', var_name='time', value_name='vals')
+g = sns.lineplot(x="x_coordinate", y="vals", hue='time', data=dfm)#, kind='point',s = 1)
+sns.lineplot(x="x_coordinate", y="vals", hue='time', data=dfm1,ax=g)#, kind='point',s = 1)
+#sns.catplot(x="x_coordinate", y="vals", hue='time', data=dfm1, kind='point',s = 1)
+
+# df_sigma_1_0_gaussTime02_t1e3_scale050 = store200["/sigma_1_0/gaussTime02/t1e4"]
+# df_sigma_1_0_gaussTime02_t1e3_scale200 = store050['/sigma_1_0/gaussTime02/t1e4']
+# for col_name in df_sigma_1_0_gaussTime02_t1e3_scale200.columns:
+#     print(col_name)
+# ax1 = sns.lineplot(data=df_sigma_1_0_gaussTime02_t1e3_scale200,x='x_coordinate',y='t_20000')
+# sns.lineplot(data=df_sigma_1_0_gaussTime02_t1e3_scale200,x='x_coordinate',y='t_20000',ax=ax1)
+#g.set_xlim(0.075,0.2)
+#plt.xlim(0.00125,0.98875)
+#plt.xlim(150, 250)
+plt.xlim(0.4, 0.6)
+#plt.xticks(np.arange(150,10,250))
 plt.show()
