529ed0e699d9bf0116f4e3fc38c9bf0a44b9503d
GiuliaFedrizzi <scgf@leed.ac.uk>
2022-07-28 12:06:27 +0100

    read csv in a function

--- build_hdf5.py
+++ build_hdf5.py
@@ -10,23 +10,39 @@ import math
 import os
 import glob
 import time
-
-#parent_directory = "/nobackup/scgf/myExperiments/gaussScale/scale050/"
+from multiprocessing import Pool # to run in parallel
 
 # some variables for the subplots:
-
 fig, axs = plt.subplots(nrows=1, ncols=1)
-
-#os.chdir(parent_directory)
 
 # initialise stuff
 max_P = 0.0
 min_P = 0.0
 
+if os.path.exists('my_h5_limitFiles0_sigma3_func.h5'):
+    os.remove('my_h5_limitFiles0_sigma3_func.h5')
 
 
-if os.path.exists('my_h5.h5'):
-    os.remove('my_h5.h5')
+# wrap csv importer in a function that can be mapped
+def read_extract_P(myExp,x_col_done):
+    """converts a filename to a pandas dataframe"""
+        # go through files, one by one
+    myExp = pd.read_csv(file, header=0)
+    # find the location in the y coordinate corresponding to the max pressure 
+    ymax = myExp.loc[myExp['Pressure'].idxmax(), 'y coord']
+    # find the x coordinates that corresponds to the max pressure
+    x_array = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['y coord'],ymax,rel_tol=1e-3),axis=1),'x coord'])
+    # extract the pressure arrays
+    pressure_array_x = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['y coord'],ymax,rel_tol=1e-3),axis=1),'Pressure'])
+    #pressure_array_y = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['x coord'],xmax,rel_tol=1e-3),axis=1),'Pressure'])
+    col_name = file.replace(".csv","").replace("my_experiment","t_")
+    if x_col_done == 0:
+        df["x_coordinate"] = x_array
+        x_col_done = 1   # now I've done it, so don't add it next time
+    df[col_name] = pressure_array_x  # store x coordinates
+    return df,x_col_done
+
+
 
 for sigma_dir in sorted(glob.glob("sigma_3*")): # sigma_1_0
     print(sigma_dir)
@@ -50,12 +66,14 @@ for sigma_dir in sorted(glob.glob("sigma_3*")): # sigma_1_0
          # ~~~ start time ~~~
             st = time.time()
             if str(os.getcwd()).endswith('_5e5') or str(os.getcwd()).endswith('e6'):  # if timestep is big (>5e5) -> many files 
-                file_list = sorted(glob.glob("my_experiment00*"))   # -> only look at the ones that start with 00
+                files = sorted(glob.glob("my_experiment00*"))   # -> only look at the ones that start with 00
             elif str(os.getcwd()).endswith('_1_5e5'):                                 # tstep = 1e5
-                file_list = sorted(glob.glob("my_experiment0*"))   # -> only look at the ones that start with 0
+                files = sorted(glob.glob("my_experiment0*"))   # -> only look at the ones that start with 0
             else:
                 print(str(os.getcwd()) + ', not a special case')
-                file_list = sorted(glob.glob("my_experiment*.csv"))   # -> only look at the ones that start with 0
+                files = sorted(glob.glob("my_experiment*.csv"))   # -> only look at the ones that start with 0
+            file_list = [filename for filename in files if filename.split('.')[1]=='csv']
+            file_list = file_list[2:23]  # take only files in this interval
          # ~~~ end time ~~~
             et = time.time()
             elapsed_time = et - st
@@ -66,27 +84,8 @@ for sigma_dir in sorted(glob.glob("sigma_3*")): # sigma_1_0
             x_col_done = 0       # have I put the column with the x coordinates in the dataframe yet?
          # ~~~ start time ~~~
             st1 = time.time()
-            for file in file_list[2:23]:
-                """ loop through FILES """
-                # if i == 0 or i == 1:
-                #     continue
-                # if i == 20:
-                #     continue
-
-                # go through files, one by one
-                myExp = pd.read_csv(file, header=0)
-                # find the location in the y coordinate corresponding to the max pressure 
-                ymax = myExp.loc[myExp['Pressure'].idxmax(), 'y coord']
-                # find the x coordinates that corresponds to the max pressure
-                x_array = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['y coord'],ymax,rel_tol=1e-3),axis=1),'x coord'])
-                # extract the pressure arrays
-                pressure_array_x = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['y coord'],ymax,rel_tol=1e-3),axis=1),'Pressure'])
-                #pressure_array_y = np.array(myExp.loc[myExp.apply(lambda x: math.isclose(x['x coord'],xmax,rel_tol=1e-3),axis=1),'Pressure'])
-                col_name = file.replace(".csv","").replace("my_experiment","t_")
-                if x_col_done == 0:
-                    df["x_coordinate"] = x_array
-                    x_col_done = 1   # now I've done it, so don't add it next time
-                df[col_name] = pressure_array_x  # store x coordinates
+            for file in file_list:
+                df,x_col_done = read_extract_P(file,x_col_done)
 
                 #df.attrs['tstep'] = tstep_dir.split("_")[2]  # get the third part of the name, after _ (eg 5e3 from tstep02_3_5e3)
             # --- end for loop files ---
@@ -103,7 +102,9 @@ for sigma_dir in sorted(glob.glob("sigma_3*")): # sigma_1_0
 
             # append df to a group and assign the key with f-strings
                                                                 #f'{group}/{k}'
-            df.to_hdf("../../../my_h5_limitFiles0_sigma3.h5", f'{sigma_dir}/{time0_name}/{tstep_name}', append=True)
+            df.to_hdf("../../../my_h5_limitFiles0_sigma3_func.h5", f'{sigma_dir}/{time0_name}/{tstep_name}', append=True)
+            # !!!! if you change the name here, remember to change it where it checks if it exists  !!!!
+
             et3 = time.time()
             elapsed_time3 = et3 - st3
             print('To append to h5:', elapsed_time3, 'seconds')
