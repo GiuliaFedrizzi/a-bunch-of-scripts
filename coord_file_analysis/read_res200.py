72f38f4e8eca92a87e0b8c09f3674b920a773738
GiuliaFedrizzi <scgf@leeds.ac.uk>
2023-05-25 18:38:59 +0100

    plt difference between rows

--- coord_file_analysis/read_res200.py
+++ coord_file_analysis/read_res200.py
@@ -6,6 +6,9 @@ it reads the "UNODES" section of the file
 import os
 import pandas as pd
 import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 os.chdir('/home/home01/scgf/myscripts/coord_file_analysis/')
     
@@ -14,8 +17,21 @@ file_name = "res200.elle"
 df = pd.read_csv(file_name,skiprows=12457,delim_whitespace=True,header=None)  # read as dataframe
 # first line = 12458 last = end of file
 
-new_names = ['id', 'x', 'y', 'conc']    # rename columns
+new_names = ['id_n', 'x', 'y', 'conc']    # rename columns
 df.columns = new_names
 
-df.drop(columns=['id','conc'], inplace=True)   # drop the first column (duplicate) and the conc column
-print(df)
+df.drop(columns=['id_n','conc'], inplace=True)   # drop the first column (duplicate) and the conc column
+
+df = df[50:len(df):200]    # dataframe only containing a vertical line. start from the 50th element and skip a row of 200 elements
+ycoord_vals = df["y"].values
+diff_1 = list(ycoord_vals[1:] - ycoord_vals[:-1])
+diff_1.append(diff_1[-1])
+df["diff"] = diff_1
+
+fig, ax1 = plt.subplots(nrows=1,ncols=1)
+
+sns.scatterplot(data=df ,x=df.index,y='diff',ax=ax1)
+sns.lineplot(data=df ,x=df.index,y='diff',ax=ax1)
+ax1.get_yaxis().get_major_formatter().set_useOffset(False)
+# sns.lineplot(data=df ,x=df.index,y='y')
+plt.show()
