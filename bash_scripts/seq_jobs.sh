1ad6197856a8556bdbd5c40d4434fcab92664ddf
GiuliaFedrizzi <scgf@leeds.ac.uk>
2023-07-07 14:01:14 +0100

    add comments

--- bash_scripts/seq_jobs.sh
+++ bash_scripts/seq_jobs.sh
@@ -3,6 +3,12 @@
 # e.g.   ./seq_jobs.sh 5430171 5430177  will wait until all jobs in the
 #        range between 5430171 and 5430177 are done, then run the new one
 
-job_names=$(echo `seq -s, $1 $2`)
+
+# generate a sequence between the first and second number
+# separated by a comma
+job_names=$(echo `seq -s, $1 $2`)  
+
 #echo $job_names
+
+# the job will be held in the queue until all $job_names are completed
 qsub -hold_jid $job_names hello1.sh
