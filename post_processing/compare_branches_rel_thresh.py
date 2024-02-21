e52a7f3f2ea297809c1ff61b35f6310c429efc0c
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-02-21 15:52:39 +0000

    plot multiple mr to compare rt

--- post_processing/compare_branches_rel_thresh.py
+++ post_processing/compare_branches_rel_thresh.py
@@ -34,6 +34,15 @@ def plot_data(root_dirs, visc, vis, row_index,custom_labels):
                         if root_dir == 'rt0.004' or root_dir == 'rt0.006' or root_dir == 'rt0.008':
                             temp_row_index *= 10
                             print(f'row_index {temp_row_index}')
+                    if vis.endswith("02"):
+                        temp_row_index = round(temp_row_index / 2)
+                        print(f'tstep = {temp_row_index} (should be {temp_row_index / 2})')
+                    if vis.endswith("05"):
+                        temp_row_index = round(temp_row_index / 5)
+                        print(f'tstep = {temp_row_index} (should be {temp_row_index / 5})')
+                    if vis.endswith("08"):
+                        temp_row_index = round(temp_row_index / 8)
+                        print(f'tstep = {temp_row_index} (should be {temp_row_index / 8})')
                     if temp_row_index < len(df):
                         x.append(label_mapping[root_dir])
                         y.append(df.loc[temp_row_index, metric])
@@ -52,25 +61,25 @@ def plot_data(root_dirs, visc, vis, row_index,custom_labels):
         axs[i].set_title(metric)
         axs[i].set_xlabel('rel thresh')
         axs[i].set_ylabel(metric)
-        # axs[i].legend()
+        axs[i].legend()
         axs[i].tick_params(axis='x', labelsize=5)
 
     # plt.tight_layout()
-    fig.suptitle(visc+"/"+vis+", tstep = "+str(temp_row_index), fontsize=16)
+    fig.suptitle(visc+", t$_{ref}$ = "+str(row_index), fontsize=16)
     plt.show()
 
 
 # root_dirs =     ['p52',    'p53',   'p50',    'p61',     'p62',    'p56',       'p63']#, 'p50']  # Add your p* directories here
 # custom_labels = ['0.001', '0.0015','0.002','0.001, 2','0.001, 4','0.001, 10','0.001, 20']    # Custom labels
-root_dirs =     ['rt0.0001',    'rt0.0002',   'rt0.0006','rt0.001','rt0.002','rt0.004','rt0.006','rt0.008']#,    'p61',     'p62',    'p56',       'p63']#, 'p50']  # Add your p* directories here
-visc = 'visc_4_1e4'
-vis_dirs = ['vis1e4_mR_09']#, 'vis1e1_mR_09']  # Add your selected vis* subdirectories here
-row_index = 4  # Specify the row index here
+root_dirs =     ['rt0.006',    'rt0.01',   'rt0.02','rt0.04']#,    'p61',     'p62',    'p56',       'p63']#, 'p50'] 
+visc = 'visc_1_1e1'#,'visc_4_1e4']
+vis_dirs = ['vis1e1_mR_02', 'vis1e1_mR_05','vis1e1_mR_08']  # Add your selected vis* subdirectories here
+row_index = 10  # Specify the row index = timestep
 
 
 custom_labels = []
 for d in root_dirs:
-    custom_labels.append(getParameterFromInput(d+"/"+visc+"/baseFiles/input.txt","rel_thresh"))   # extract the value for relaxation threshold
+    custom_labels.append(getParameterFromInput(d+"/"+visc+"/baseFiles/input.txt","second_rel_thr"))   # extract the value for relaxation threshold
 print(f'rel_thresh {custom_labels}')
 
 # custom_labels = ['0.001', '0.0015','0.002']#,'0.001, 2','0.001, 4','0.001, 10','0.001, 20']    # Custom labels
@@ -79,5 +88,5 @@ if len(root_dirs) != len(custom_labels):
     custom_labels = root_dirs
 
 
-
+# for visc in viscs:
 plot_data(root_dirs, visc, vis_dirs, row_index, custom_labels)
