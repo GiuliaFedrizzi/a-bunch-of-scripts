172355a9fa7f0ae4f59acfab0f9b382f7229bc4e
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-04-03 10:18:19 +0100

    change format

--- post_processing/get_porosity_range.py
+++ post_processing/get_porosity_range.py
@@ -22,7 +22,7 @@ def extract_min_max_porosity(directories):
     all_poro_min =[]
     all_poro_max =[]
     for directory in directories:
-        csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+        csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]  # AND it starts with my_exp   !!! 
         if csv_files:
             # Sort and select the last CSV file
             last_csv_file = sorted(csv_files,key=extract_number)[-1]
@@ -36,18 +36,13 @@ def extract_min_max_porosity(directories):
 
     return min(all_poro_min),max(all_poro_max)
 
-# Define the root directory of your directory tree
-root_dir = os.getcwd()
-
 # Find all innermost directories containing CSV files
-innermost_dirs = find_innermost_directories(root_dir)
+innermost_dirs = find_innermost_directories(os.getcwd())
 
 # Extract the minimum and maximum porosity values
 porosity_values = extract_min_max_porosity(innermost_dirs)
-print(f'Global porosity range {porosity_values}')
+
+# save to file. This file will be copied into the directories
 f = open("global_porosity_range.txt", "w")
-f.write(str(porosity_values))
+f.write(str(porosity_values[0])+"\n"+str(porosity_values[1]))
 f.close()
-# # Print or further process the extracted values
-# for file_path, min_max_values in porosity_values.items():
-#     print(f'{file_path}: Min Porosity = {min_max_values[0]}, Max Porosity = {min_max_values[1]}')
