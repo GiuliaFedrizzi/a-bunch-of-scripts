2d5d741c89ff61d59b54ec631f075f3f328f60e3
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-05-24 11:11:43 +0100

    move extract_two_profiles to useful_functions

--- post_processing/u_phi_profile.py
+++ post_processing/u_phi_profile.py
@@ -1,42 +1,29 @@
 """
 plot u*phi as a measure of flux leaving the domain
 """
-import pandas as pd
+# import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 from pathlib import Path
+import sys
+sys.path.append('/home/home01/scgf/myscripts/post_processing')
+from useful_functions import extract_two_profiles,getResolution
 
 
 
-res = 200    # resolution in the x direction
-res_y = 230  # resolution in the y direction
+res = getResolution()    # resolution in the x direction
+res_y = int(res*1.15)  # resolution in the y direction
 filename = "my_experiment16000.csv"
-first_part_of_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_3_1e3/vis1e3_mR_08'
-
-
-dir = first_part_of_path
+dir_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_3_1e3/vis1e3_mR_08'
 
 # define the indexes for the two profiles, the first one is the start for the horizontal, the second for the vertical profile
 # point_indexes = [res*res_y-res,5]   # X original: res*res_y/2+res, central: res*res_y/2 (?)
 point_indexes = [(res*res_y*0.75+res),5]   # 3/4 of the domain
 print(f'index for the horizontal profile: {point_indexes[0]}')
-def read_calculate_plot(filename, var_to_plot,point_indexes):
-
-    myExp = pd.read_csv(filename, header=0)
-    # df_v = myExp[50:len(myExp):res]    # dataframe only containing a vertical line. start from the 50th element and skip 2 rows of 200 elements
-    df_v = myExp[int(point_indexes[1]):len(myExp):int(res)]    # dataframe only containing a vertical line. start from the n-th element and skip 2 rows of 200 elements
-    hor_index_start = int(point_indexes[0])  # index for the horizontal profile (constant y)
-    df_h = myExp[hor_index_start:hor_index_start+res:1]    # dataframe only containing a horizontal line. start from the left boundary, continue for 200 elements
-    # print(len(df_v))
-    variable_vals_v = df_v[var_to_plot].values
-    variable_vals_h = df_h[var_to_plot].values
-    x_v = np.arange(0,1,1/len(df_v))
-    x_h = np.arange(0,1,1/len(df_h))
-    return (x_v, variable_vals_v), (x_h, variable_vals_h)
 
 
-os.chdir(dir)
+os.chdir(dir_path)
 myfile = Path(os.getcwd()+'/'+filename)  # build file name including path
 
 
@@ -50,7 +37,7 @@ all_data_v = {}
 all_data_h = {}
 
 for v in vars_to_plot:
-    (x_v, y_v), (x_h, y_h) = read_calculate_plot(filename, v,point_indexes)
+    (x_v, y_v), (x_h, y_h) = extract_two_profiles(filename, v,point_indexes)
     all_data_v[v] = (x_v, y_v)
     all_data_h[v] = (x_h, y_h)
 # print(f'all_data_v \n{all_data_v}')
@@ -95,8 +82,6 @@ if False:
     ax2.legend([line1_v, line2_v], 
             [vars_to_plot[0], vars_to_plot[2]],loc=(0.8, 0.9))
 
-    plt.show()
-
 if True:
     """
     plot the product: u*phi
@@ -114,4 +99,7 @@ if True:
     line1_v, = ax2.plot(y_vel_x*y_poro,x)
     ax2.set_xlabel('$\phi * u_x$') 
     ax2.set_ylabel('y') 
-    plt.show()
+
+
+plt.show()
+
