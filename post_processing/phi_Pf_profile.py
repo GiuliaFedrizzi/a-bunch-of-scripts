dcf0c31531b17f3a7a3191375b740ff01f4f141d
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-05-28 17:47:46 +0100

    fill between Pf and phi locations in the plot

--- post_processing/phi_Pf_profile.py
+++ post_processing/phi_Pf_profile.py
@@ -15,7 +15,7 @@ from useful_functions import extract_two_profiles,getResolution
 
 res = getResolution()    # resolution in the x direction
 res_y = int(res*1.15)  # resolution in the y direction
-filename = "my_experiment16000.csv"
+filename = "my_experiment03000.csv"
 dir_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_3_1e3/vis1e3_mR_08'
 
 # define the indexes for the two profiles, the first one is the start for the horizontal, the second for the vertical profile
@@ -29,7 +29,7 @@ os.chdir(dir_path)
 myfile = Path(os.getcwd()+'/'+filename)  # build file name including path
 
 
-fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2)
+fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2,figsize=(15, 8))
 
 vars_to_plot = ["Pressure","Porosity", "Permeability"]
 
@@ -50,38 +50,47 @@ ax1b = ax1.twinx()
 line1_h, = ax1.plot(all_data_h[vars_to_plot[0]][0], all_data_h[vars_to_plot[0]][1])
 line2_h, = ax1a.plot(all_data_h[vars_to_plot[1]][0], all_data_h[vars_to_plot[1]][1],'g')  # plot porosity in green
 line3_h, = ax1b.plot(all_data_h[vars_to_plot[2]][0], all_data_h[vars_to_plot[2]][1],color='lightgray',alpha=0.3)  # plot permeability
-phi_scale_hor = max(all_data_h[vars_to_plot[0]][1])/max(all_data_h[vars_to_plot[1]][1])  # (max Pf)/(max phi) because if I want to plot them on the same y axis they have to be comparable
-print(f'phi_scale_hor {phi_scale_hor}')
-ax1.fill_between(all_data_h[vars_to_plot[0]][0], all_data_h[vars_to_plot[0]][1],all_data_h[vars_to_plot[1]][1]*phi_scale_hor,interpolate=True,alpha=0.2)  # x, y1, y2
+
 ax1b.fill_between(all_data_h[vars_to_plot[2]][0], all_data_h[vars_to_plot[2]][1],y2=0,color='lightgray',alpha=0.3)  # fill between 0 and permeability 
 ax1b.set_yscale('log')
 
 # Legend
 ax1.legend([line1_h, line2_h, line3_h], 
-        [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.6, 0.9))   # labels: y velocity and poro
+        [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.6, 0.8))   # labels: y velocity and poro
 
 ax1.set_xlabel('x') 
 ax1.set_ylabel(vars_to_plot[0]) 
-# ax1.set_ylim([6.6e7,1.09e8])  # Pf
-# ax1a.set_ylim([0.13,0.32])    # phi
+ax1.set_ylim([6.6e7,1.09e8])  # Pf
+ax1a.set_ylim([0.135,0.3])    # phi
+ax1b.set_ylim([7.37016e-19,1.66541e-16])    # k
 ax1a.set_ylabel(vars_to_plot[1], color='g')  # Setting color to match line color
 ax1a.yaxis.tick_right()  # Ensure the y-axis label is on the right
 ax1a.tick_params(axis='y', colors='g')
+ax1.set_xlim([0,1])
+
+# transform datasets to display space
+# WARNING it only works if I set the limits for the axes manually
+x1p, y1p = ax1.transData.transform(np.c_[all_data_h[vars_to_plot[0]][0],all_data_h[vars_to_plot[0]][1]]).T
+_, y2p = ax1a.transData.transform(np.c_[all_data_h[vars_to_plot[1]][0],all_data_h[vars_to_plot[1]][1]]).T
+
+ax1.autoscale(False)
+ax1.fill_between(x1p, y1p, y2p, color='teal',alpha=0.2, transform=None)
+
 
 # Plotting on ax2
 ax2a = ax2.twiny()
 ax2b = ax2.twiny()
 
+ax2.set_ylim([0,0.99])
+ax2.set_xlim([7.3e7,1.1e8])  # Pf
+ax2a.set_xlim([0.134,0.30])  # phi
+ax2b.set_xlim([7.22131e-19,1.66186e-16])    # k
+
+
 line1_v, = ax2.plot(all_data_v[vars_to_plot[0]][1], all_data_v[vars_to_plot[0]][0], label=vars_to_plot[0])
 line2_v, = ax2a.plot(all_data_v[vars_to_plot[1]][1], all_data_v[vars_to_plot[1]][0],'g', label=vars_to_plot[1])
 line3_v, = ax2b.plot(all_data_v[vars_to_plot[2]][1], all_data_v[vars_to_plot[2]][0],color='lightgray',alpha=0.3)
 
-print(f'max Pf {max(all_data_v[vars_to_plot[0]][1])}')
-print(f'max phi {max(all_data_v[vars_to_plot[1]][1])}')
-phi_scale_ver = max(all_data_v[vars_to_plot[0]][1])/max(all_data_v[vars_to_plot[1]][1])  # (max Pf)/(max phi) because if I want to plot them on the same y axis they have to be comparable
-print(f'phi_scale_ver {phi_scale_ver}')
-
-ax2.fill_betweenx(all_data_v[vars_to_plot[1]][0], all_data_v[vars_to_plot[0]][1],all_data_v[vars_to_plot[1]][1]*phi_scale_ver,interpolate=True,alpha=0.2)  # y,x1,x2   between pressure and porosity 
 ax2b.fill_betweenx(all_data_v[vars_to_plot[2]][0], all_data_v[vars_to_plot[2]][1],x2=0,color='lightgray',alpha=0.3)  # y,x1,x2    between 0 and permeability 
 ax2b.set_xscale('log')
 
@@ -92,7 +101,15 @@ ax2a.tick_params(axis='x', colors='g')
 
 # Legend
 ax2.legend([line1_v, line2_v,line3_v], 
-        [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.8, 0.9))
+        [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.7, 0.8))
+
+# transform datasets to display space
+x1p, yp = ax2.transData.transform(np.c_[all_data_v[vars_to_plot[0]][1],all_data_v[vars_to_plot[0]][0]]).T
+x2p, _ = ax2a.transData.transform(np.c_[all_data_v[vars_to_plot[1]][1],all_data_v[vars_to_plot[1]][0]]).T
+ax2.autoscale(False)
+ax2.fill_betweenx(yp, x1p, x2p, color="teal", alpha=0.2, transform=None)
+
+# print(f'horizontal min {min(all_data_h[vars_to_plot[2]][1])}, max {max(all_data_h[vars_to_plot[2]][1])}, \nvertical min {min(all_data_v[vars_to_plot[2]][1])} max {max(all_data_v[vars_to_plot[2]][1])}')
 
 fig.suptitle(str(myfile))
 plt.show()
