decf00f52c90351339336d6cfe457ba36a80bb28
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-05-29 17:51:32 +0100

    take average of 5 lines

--- post_processing/u_phi_anisotropy.py
+++ post_processing/u_phi_anisotropy.py
@@ -10,7 +10,7 @@ import os
 from pathlib import Path
 import sys
 sys.path.append('/home/home01/scgf/myscripts/post_processing')
-from useful_functions import extract_two_profiles,getResolution
+from useful_functions import extract_two_profiles,getResolution,average_flow_ratio
 
 
 
@@ -24,10 +24,14 @@ dir_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_3_
 
 # define the indexes for the two profiles, the first one is the start for the horizontal, the second for the vertical profile
 # REMEMBER to remove 1 from the horizontal index
-point_indexes = [(res*res_y-(5*res))-1,5]   # top (horizontal) and left (vertical). X original: res*res_y/2+res, central: res*res_y/2 (?)
+# point_indexes = [(res*res_y-(5*res))-1,5]   # top (horizontal) and left (vertical). X original: res*res_y/2+res, central: res*res_y/2 (?)
 # point_indexes = [(res*res_y*0.75+res),5]   # 3/4 of the domain
-# print(f'index for the horizontal profile: {point_indexes[0]}')
 
+hor_points = [(res*res_y-(5*res))-1,
+    (res*res_y-(10*res))-1,
+    (res*res_y-(15*res))-1,
+    (res*res_y-(20*res))-1]
+ver_points = [5,10,15,20]
 
 os.chdir(dir_path)
 
@@ -39,45 +43,23 @@ ax1a = ax1.twinx()
 
 vars_to_plot = ["x velocity","y velocity", "Porosity"]
 
+
 int_ratios = []
 int_hor_list = []
 int_vert_list = []
 
 for filename in filenames:
     print(filename)
-    # prepare to store vertical and horizontal data
-    all_data_v = {}
-    all_data_h = {}
-
-    for v in vars_to_plot:
-        (x_v, y_v), (x_h, y_h) = extract_two_profiles(filename, v,point_indexes)
-        all_data_v[v] = (x_v, y_v)
-        all_data_h[v] = (x_h, y_h)
-        
-
-    x_coord_vel_hor, y_vel_hor = all_data_h[vars_to_plot[1]] 
-    x, poro_values_hor = all_data_h[vars_to_plot[2]]
-
-
-    integral_hor = np.trapz((y_vel_hor), x=x_coord_vel_hor)
-    print(f'integral_hor {integral_hor}')
-
-    y_coord_vel_ver, x_vel_ver = all_data_v[vars_to_plot[0]] 
-    x, poro_values_ver = all_data_v[vars_to_plot[2]]
-    integral_ver =  np.trapz((x_vel_ver*poro_values_ver), x = y_coord_vel_ver)
-
-    int_ratio = integral_hor/integral_ver
-
-    print(f'integral_ver {integral_ver}')
-    print(f'integral ratio {int_ratio}')
+    average_hor,average_ver,int_ratio = average_flow_ratio(filename,ver_points,hor_points,vars_to_plot)
     int_ratios.append(int_ratio)
-    int_hor_list.append(integral_hor)
-    int_vert_list.append(integral_ver)
+    int_hor_list.append(average_hor)
+    int_vert_list.append(average_ver)
     
+ax1.axhline(y=0.0,color='k',linestyle='--',alpha=0.4)
 ax1a.axhline(y=0.0,color='grey',linestyle=':',alpha=0.5)
 p1 = ax1.plot(file_tsteps,int_ratios,'o-',linewidth=2,color='black',label='Ratio')
-p2 = ax1a.plot(file_tsteps,int_hor_list,'o--',label='Top Vol Flow Rate')
-p3 = ax1a.plot(file_tsteps,int_vert_list,'o--',label='Side Vol Flow Rate')
+p2 = ax1a.plot(file_tsteps,int_hor_list,'o--',label='Top Vol Flow Rate, $\phi u_y$')
+p3 = ax1a.plot(file_tsteps,int_vert_list,'o--',label='Side Vol Flow Rate, $\phi u_x$')
 ax1.set_xlabel("File Number")
 ax1.set_ylabel("$\mathbf{Q_{top}/Q_{side}}$ Ratio",fontweight='bold')
 ax1a.set_ylabel("$\phi u_i $")
