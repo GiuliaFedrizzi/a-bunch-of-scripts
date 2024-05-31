ba703025b18f4b3a43b8f306d9032382c08b0598
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-05-31 09:04:19 +0100

    whole domain only

--- post_processing/porosity_average.py
+++ post_processing/porosity_average.py
@@ -20,28 +20,29 @@ def get_order(file):
 sortedInputFiles = sorted(glob.glob('my_experiment*.csv'), key=get_order)   # order files based on their full number, found with get_order()
 
 input_tstep = float(getTimeStep("input.txt"))
-for i,filename in enumerate(sortedInputFiles[0:25:5]): #[beg:end:step]  set which timesteps (based on FILE NUMBER) 
+for i,filename in enumerate(sortedInputFiles[0:20:1]): #[beg:end:step]  set which timesteps (based on FILE NUMBER) 
     myfile = Path(os.getcwd()+'/'+filename)
     if myfile.is_file():
         myExp = pd.read_csv(filename, header=0)
         # average everywhere
         average_porosity.append(myExp['Porosity'].mean()*100)
         # average in the melting area only
-        average_porosity_meltArea.append(myExp[myExp["y coord"]<0.1]["Porosity"].mean()*100)  # mean with conditions on x and y coordinates
+        # average_porosity_meltArea.append(myExp[myExp["y coord"]<0.1]["Porosity"].mean()*100)  # mean with conditions on x and y coordinates
         file_num = float(filename.split("experiment")[1].split(".")[0])  # first take the part after "experiment", then the one before the "."
         time_array.append(file_num*input_tstep)
 
 # calculate rate of porosity increase (final poro - initial poro over total time)
 # rate = (time_array[-1]-time_array[0])/(average_porosity[-1]-average_porosity[0])
 rate = (average_porosity[-1]-average_porosity[0])/(time_array[-1]-time_array[0])
-# print(average_porosity[-1]-average_porosity[0])
+print(average_porosity[-1]-average_porosity[0])
+print(time_array[-1]-time_array[0])
 
 s_in_year = 60*60*24*365
 print("rate = ",str(rate*s_in_year),"% per year")
 
 
 plt.scatter(time_array,average_porosity,label='Everywhere')
-plt.scatter(time_array,average_porosity_meltArea,label='Melting area')
+# plt.scatter(time_array,average_porosity_meltArea,label='Melting area')
 plt.xlabel("Time (s)")
 plt.ylabel("Average Porosity, %")
 plt.legend()
