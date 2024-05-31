43867b32214a96e17c67841be9b6a7b7429455ef
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-05-31 16:02:34 +0100

    format plots

--- post_processing/phi_Pf_profile.py
+++ post_processing/phi_Pf_profile.py
@@ -12,10 +12,7 @@ sys.path.append('/home/home01/scgf/myscripts/post_processing')
 from useful_functions import extract_two_profiles,getResolution
 
 
-
-res = getResolution()    # resolution in the x direction
-res_y = int(res*1.15)  # resolution in the y direction
-filenames = ["my_experiment01000.csv","my_experiment03000.csv","my_experiment07000.csv","my_experiment16000.csv"]
+file_numbers = ["01000","03000","07000","16000"]
 # dir_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_1_1e15/vis1e15_mR_03'
 dir_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/prt45/rt0.5/visc_3_1e3/vis1e3_mR_08'
 
@@ -25,8 +22,14 @@ Pf_axes_lim = [6.6e7,1.6e8]
 # phi_axes_lim = [0.134,0.30]  # case A
 phi_axes_lim = [0.10,0.42]
 # k_axes_lim = [7.22131e-19,1.66541e-16]  # case A
-k_axes_lim = [7.22131e-19,3.2e-16]
+k_axes_lim = [-1e-18,3.2e-16]
 
+label_size = 13
+
+os.chdir(dir_path)
+
+res = getResolution()    # resolution in the x direction
+res_y = int(res*1.15)  # resolution in the y direction
 # define the indexes for the two profiles, the first one is the start for the horizontal, the second for the vertical profile
 # REMEMBER to remove 1 from the horizontal index
 point_indexes = [(res*res_y-(5*res))-1,5]   # top (horizontal) and left (vertical). X original: res*res_y/2+res, central: res*res_y/2 (?)
@@ -34,13 +37,21 @@ point_indexes = [(res*res_y-(5*res))-1,5]   # top (horizontal) and left (vertica
 # print(f'index for the horizontal profile: {point_indexes[0]}')
 
 
-os.chdir(dir_path)
-
-for filename in filenames:
+for filenum in file_numbers:
+        filename = "my_experiment"+filenum+".csv"
         myfile = Path(os.getcwd()+'/'+filename)  # build file name including path
 
 
-        fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2,figsize=(17, 8))
+        plt.rcParams["font.weight"] = "bold"
+        plt.rcParams['lines.linewidth'] = 4
+        plt.rcParams['axes.linewidth'] = 2
+        plt.rcParams['axes.labelsize'] = 20
+        plt.rcParams['xtick.labelsize'] = 18
+        plt.rcParams['ytick.labelsize'] = 18
+
+        fig, (ax1,ax2) = plt.subplots(nrows=1, ncols=2,figsize=(17, 9))
+        plt.setp(ax1.spines.values(), linewidth=3)  # #f56c42
+        plt.setp(ax2.spines.values(), linewidth=3)  # #9ce35d
 
         vars_to_plot = ["Pressure","Porosity", "Permeability"]
 
@@ -50,7 +61,7 @@ for filename in filenames:
         all_data_h = {}
 
         for v in vars_to_plot:
-                (x_v, y_v), (x_h, y_h) = extract_two_profiles(filename, v,point_indexes)
+                (x_v, y_v), (x_h, y_h) = extract_two_profiles(filename, v,point_indexes,res)
                 all_data_v[v] = (x_v, y_v)
                 all_data_h[v] = (x_h, y_h)
         
@@ -66,29 +77,33 @@ for filename in filenames:
         # ax1b.set_yscale('log')
 
         # Legend
-        ax1.legend([line1_h, line2_h, line3_h], 
-                [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.6, 0.8))   # labels: y velocity and poro
+        # ax1.legend([line1_h, line2_h, line3_h], 
+                # [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.6, 0.8))   # labels: y velocity and poro
 
-        ax1.set_xlabel('x') 
-        ax1.set_ylabel(vars_to_plot[0]) 
+        #   limits:
+        # ax1.set_ylabel(vars_to_plot[0]) 
         ax1.set_ylim(Pf_axes_lim)  # Pf
         ax1a.set_ylim(phi_axes_lim)    # phi
         ax1b.set_ylim(k_axes_lim)    # k
-        ax1a.set_ylabel(vars_to_plot[1], color='g')  # Setting color to match line color
-        ax1a.yaxis.tick_right()  # Ensure the y-axis label is on the right
-        ax1a.tick_params(axis='y', colors='g')
         ax1.set_xlim([0,1])
+        # ax1a.set_ylabel(vars_to_plot[1], color='g')  # Setting color to match line color
+        # ax1.set_xlabel('x') 
 
-        # transform datasets to display space
-        # WARNING it only works if I set the limits for the axes manually
-        x1p, y1p = ax1.transData.transform(np.c_[all_data_h[vars_to_plot[0]][0],all_data_h[vars_to_plot[0]][1]]).T
-        _, y2p = ax1a.transData.transform(np.c_[all_data_h[vars_to_plot[1]][0],all_data_h[vars_to_plot[1]][1]]).T
+        #   options:
+        ax1a.yaxis.tick_right()  # Ensure the y-axis label is on the right
+        ax1.tick_params(axis='x')
+        ax1.tick_params(axis='y', colors='#1f77b4')
+        ax1.xaxis.set_major_locator(plt.MaxNLocator(4))
+        ax1.yaxis.set_major_locator(plt.MaxNLocator(4))
+        ax1a.yaxis.set_major_locator(plt.MaxNLocator(4))
+        ax1b.yaxis.set_major_locator(plt.MaxNLocator(4))
+        ax1a.tick_params(axis='y', colors='g')
+        ax1b.tick_params(axis='y', colors='darkgray') 
+        ax1b.spines['right'].set_position(('outward', 60))  # draw the axis for k further from the axis for phi
 
-        ax1.autoscale(False)
-        ax1.fill_between(x1p, y1p, y2p, color='teal',alpha=0.2, transform=None)
 
 
-        # Plotting on ax2
+        # Plotting on ax2 -- vertical profile ---
         ax2a = ax2.twiny()
         ax2b = ax2.twiny()
 
@@ -108,24 +123,44 @@ for filename in filenames:
 
         ax2b.fill_betweenx(all_data_v[vars_to_plot[2]][0], all_data_v[vars_to_plot[2]][1],x2=0,color='lightgray',alpha=0.3)  # y,x1,x2    between 0 and permeability 
         # ax2b.set_xscale('log')
+        ax2b.spines['top'].set_position(('outward', 30))  # draw the axis for k further from the axis for phi
 
-        ax2.set_ylabel('y')
-        ax2.set_xlabel(vars_to_plot[0])
-        ax2a.set_xlabel(vars_to_plot[1], color='g')  # Setting color to match line color
+        # ax2.set_ylabel('y')
+        # ax2.set_xlabel(vars_to_plot[0])
+        # ax2a.set_xlabel(vars_to_plot[1], color='g')  # Setting color to match line color
+        ax2.tick_params(axis='x', colors='#1f77b4')
+        ax2.tick_params(axis='y') 
+        ax2.tick_params(axis='x') 
         ax2a.tick_params(axis='x', colors='g') 
-
+        ax2b.tick_params(axis='x', colors='darkgray') 
+        ax2.xaxis.set_major_locator(plt.MaxNLocator(4))
+        ax2.yaxis.set_major_locator(plt.MaxNLocator(4))
+        ax2a.xaxis.set_major_locator(plt.MaxNLocator(4))
+        ax2b.xaxis.set_major_locator(plt.MaxNLocator(4))
         # Legend
-        ax2.legend([line1_v, line2_v,line3_v], 
-                [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.7, 0.8))
+        # ax2.legend([line1_v, line2_v,line3_v], 
+                # [vars_to_plot[0], vars_to_plot[1],vars_to_plot[2]],loc=(0.7, 0.8))
 
+        fig.tight_layout()  # must go before fill_between
+
+        # fill between phi and Pf
         # transform datasets to display space
+        # WARNING it only works if I set the limits for the axes manually
+        x1p, y1p = ax1.transData.transform(np.c_[all_data_h[vars_to_plot[0]][0],all_data_h[vars_to_plot[0]][1]]).T
+        _, y2p = ax1a.transData.transform(np.c_[all_data_h[vars_to_plot[1]][0],all_data_h[vars_to_plot[1]][1]]).T
+
+        ax1.autoscale(False)
+        ax1.fill_between(x1p, y1p, y2p, color='teal',alpha=0.2, transform=None)
+        
         x1p, yp = ax2.transData.transform(np.c_[all_data_v[vars_to_plot[0]][1],all_data_v[vars_to_plot[0]][0]]).T
         x2p, _ = ax2a.transData.transform(np.c_[all_data_v[vars_to_plot[1]][1],all_data_v[vars_to_plot[1]][0]]).T
         ax2.autoscale(False)
         ax2.fill_betweenx(yp, x1p, x2p, color="teal", alpha=0.2, transform=None)
 
-        print(f'horizontal min {min(all_data_h[vars_to_plot[2]][1])}, max {max(all_data_h[vars_to_plot[2]][1])}, \nvertical min {min(all_data_v[vars_to_plot[2]][1])}, max {max(all_data_v[vars_to_plot[2]][1])}')
+        # print(f'horizontal min {min(all_data_h[vars_to_plot[2]][1])}, max {max(all_data_h[vars_to_plot[2]][1])}, \nvertical min {min(all_data_v[vars_to_plot[2]][1])}, max {max(all_data_v[vars_to_plot[2]][1])}')
 
-        fig.suptitle(str(myfile))
-plt.show()
+        # fig.suptitle(str(myfile))
+        plt.savefig("phi_Pf_"+str(filenum)+".png")
+print("Done :)")
+# plt.show()
 
