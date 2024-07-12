75c7ea0af1d6f346243e5b76af6b0380d5d26c01
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-07-12 02:43:29 +0100

    epsilon 0.00995

--- post_processing/image_analysis/dbscan_single.py
+++ post_processing/image_analysis/dbscan_single.py
@@ -13,16 +13,16 @@ import sys
 from sklearn.neighbors import NearestNeighbors
 
 no_margins = 1
-plot_figures = 0
+plot_figures = 1
 # t = 100000
-t = 11000
-os.chdir("rt0.5/visc_3_1e3/")
+t = 11000 # true time?
+os.chdir("rt0.5/pdef9e8/")
 # os.chdir("rt0.5/visc_1_1e15/")  # change  x_value and melt_value too
 file_to_read = "my_experiment"+str(t)+".csv"
-x_value = "1e3"      # <----------
-melt_value = 0.008    # <----------
+x_value = "9e8"      # <----------
+melt_value = 0.009    # <----------
 # data = read_bb_data_from_csv("vis1e3_mR_08/"+file_to_read)
-data = read_bb_data_from_csv("vis1e3_mR_08/"+file_to_read)
+data = read_bb_data_from_csv("vis1e2_mR09/"+file_to_read)
 
 if data.empty:
     print("empty df") 
@@ -37,13 +37,15 @@ if no_margins:
 X = X[['x coord','y coord']] # Only x and y coordinates 
 if len(X) > 0:
     cluster_df,ellipses_df = dbscan_and_plot(X,"viscosity",x_value,melt_value,no_margins,plot_figures,t)
+    ellipses_df['average_angle_from_90'] = abs(90-ellipses_df['Angle_Degrees'])
     print(ellipses_df)
-    ax = draw_rose_ellipse(ellipses_df)
-    # plt.show()
-    plt.savefig("../cluster/rose_"+str(x_value)+"_"+str(melt_value)+"_"+str(t)+".png",transparent=True)
+    if True:
+        ax = draw_rose_ellipse(ellipses_df)
+        plt.show()
+        plt.savefig("../cluster/rose_"+str(x_value)+"_"+str(melt_value)+"_"+str(t)+".png",transparent=True)
 
 
-if False:
+if True:
     min_samples = 10
     # Use NearestNeighbors to find the distance to the k-th nearest neighbor
     k = min_samples  # Same as your DBSCAN min_samples value
@@ -68,15 +70,16 @@ if False:
 
     plt.figure(figsize=(10, 8))
     plt.plot(distances)
-    plt.xlabel('Points sorted by distance to the '+str(min_samples)+'th nearest neighbor',fontsize=15,weight='bold')
+    plt.xlabel('Points sorted by distance to the '+str(min_samples)+'th nearest neighbour',fontsize=15,weight='bold')
     plt.ylabel("$\epsilon$",fontsize=15)
-    plt.xlim([0,25000])
+    # plt.xlim([0,25000])
     plt.ylim([0.008,0.02])
-    plt.axhline(y=0.0088,color='silver',linestyle='--')
+    # plt.axhline(y=0.0088,color='silver',linestyle='--')
+    plt.axhline(y=0.00995,color='silver',linestyle='--')  # line to show where the chosen epsilon is
     # plt.title(str(min_samples)+'th Nearest Neighbor Distance vs. Points')
     plt.grid(True,which='both')
-    # plt.show()
-    plt.savefig("../cluster/dbscan_kNN_epsion.png")
+    plt.show()
+    # plt.savefig("../cluster/dbscan_kNN_epsion.png")
 
 
 # print(cluster_df)
