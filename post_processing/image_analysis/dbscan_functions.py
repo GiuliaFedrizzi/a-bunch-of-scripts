6ec17dbea98772fa59c0d2cbbeb18925e7bdff71
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-07-12 02:46:19 +0100

    e=0.01, move if plot_figures

--- post_processing/image_analysis/dbscan_functions.py
+++ post_processing/image_analysis/dbscan_functions.py
@@ -21,7 +21,7 @@ def read_bb_data_from_csv(file_path):
 
 def dbscan_and_plot(X,variab,x_value,melt_value,no_margins,plot_figures,t):
     min_samples = 10
-    e = 0.0088
+    e = 0.01
     dbscan = DBSCAN(eps=e, min_samples=min_samples).fit(X)
     X['cluster'] = dbscan.labels_
     X=X[X['cluster']>-1]  # remove noise cluster
@@ -85,16 +85,13 @@ def dbscan_and_plot(X,variab,x_value,melt_value,no_margins,plot_figures,t):
                 largest_eigenvector = eigenvectors[:,0]  # take the largest
                 angle = np.arctan2(largest_eigenvector[1], largest_eigenvector[0]) # arctan is the angle in radians
                 angle_degrees = np.degrees(angle)
-                theta = np.linspace(0, 2*np.pi, 1000);
-                # draw an ellipse:
-                eigenvalues_draw = eigenvalues*3; eigenvectors_draw = eigenvectors
-                ellipsis = (np.sqrt(eigenvalues_draw[None,:]) * eigenvectors_draw) @ [np.sin(theta), np.cos(theta)] # parametric form
                 if plot_figures:
+                    # for each cluster, draw an ellipse
+                    theta = np.linspace(0, 2*np.pi, 1000);
+                    # draw an ellipse:
+                    eigenvalues_draw = eigenvalues*3; eigenvectors_draw = eigenvectors
+                    ellipsis = (np.sqrt(eigenvalues_draw[None,:]) * eigenvectors_draw) @ [np.sin(theta), np.cos(theta)] # parametric form
                     plt.plot(ellipsis[0,:]+centroid['x coord'], ellipsis[1,:]+centroid['y coord'],'k')
-                    if no_margins:
-                        plt.savefig("../cluster/ellipses_x_v"+str(x_value)+"_mr"+str(melt_value)+"_"+str(t)+".png")  # show all of them at the end
-                    else:
-                        plt.savefig("../cluster/ellipses_v"+str(x_value)+"_mr"+str(melt_value)+"_real_t_"+str(t)+".png")  # show all of them at the end
 
                 # Store the eigenvalues in the dictionary
                 moments_of_inertia[cluster_label] = eigenvalues
@@ -102,6 +99,12 @@ def dbscan_and_plot(X,variab,x_value,melt_value,no_margins,plot_figures,t):
                 all_elong[cluster_label] = eigenvalues[0]/eigenvalues[1]
                 all_angles[cluster_label] = angle_degrees
                 all_sizes[cluster_label] = len(cluster_points)+1
+            if plot_figures:
+                # plt.show()
+                if no_margins:
+                    plt.savefig("../cluster/ellipses_x_v"+str(x_value)+"_mr"+str(melt_value)+"_"+str(t)+"_e="+str(e)+".png")  # show all of them at the end
+                else:
+                    plt.savefig("../cluster/ellipses_v"+str(x_value)+"_mr"+str(melt_value)+"_real_t_"+str(t)+".png")  # show all of them at the end
             
             ellipses_df = pd.DataFrame({
                 "Moments_of_Inertia": pd.Series(moments_of_inertia),
@@ -110,7 +113,7 @@ def dbscan_and_plot(X,variab,x_value,melt_value,no_margins,plot_figures,t):
                 "Angle_Degrees": pd.Series(all_angles),
                 "Size": pd.Series(all_sizes)
             })
-            # print(ellipses_df)
+            print(ellipses_df)
             # if len(ellipses_df) > 2:
             #     anova_analysis(ellipses_df)
 
