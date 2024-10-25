1a4dba02a7b379d0941f33981ae53e1de6c70cb8
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-10-25 21:33:14 +0100

    build dataframe 1 tstep

--- post_processing/image_analysis/layer_non_layer.py
+++ post_processing/image_analysis/layer_non_layer.py
@@ -2,38 +2,80 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import glob
+import os
+import sys
+
+sys.path.append("/home/home01/scgf/myscripts/post_processing")
+from viz_functions import find_dirs,find_variab
+from useful_functions import getParameterFromLatte
 
 
 filename = "my_experiment09000.csv"
-df = pd.read_csv(filename, header=0)  # build the dataframe from the csv file
 
-df = df[(df["x coord"]>0.02) & (df["x coord"]<0.98)]#["Broken Bonds"]
+def get_bb_slow_and_fast(filename):
+    df = pd.read_csv(filename, header=0)  # build the dataframe from the csv file
 
-bb_df_1 = df[(df["y coord"]>0.0) & (df["y coord"]<0.1428) & (df["Broken Bonds"]>0) ]
-bb_df_2 = df[(df["y coord"]>0.1428) & (df["y coord"]<0.2857)& (df["Broken Bonds"]>0) ]
-bb_df_3 = df[(df["y coord"]>0.2857) & (df["y coord"]<0.428) & (df["Broken Bonds"]>0) ]
-bb_df_4 = df[(df["y coord"]>0.428) & (df["y coord"]<0.57) & (df["Broken Bonds"]>0) ]
-bb_df_5 = df[(df["y coord"]>0.57) & (df["y coord"]<0.714) & (df["Broken Bonds"]>0) ]
-bb_df_6 = df[(df["y coord"]>0.714) & (df["y coord"]<0.857) & (df["Broken Bonds"]>0) ]
-bb_df_7 = df[(df["y coord"]>0.857) & (df["y coord"]<0.98) & (df["Broken Bonds"]>0) ]
+    df = df[(df["x coord"]>0.02) & (df["x coord"]<0.98)]#["Broken Bonds"]
 
-# bb_fast = 
-def ratio(df):
-    return df["Broken Bonds"].sum()/len(df)
+    bb_df_1 = df[(df["y coord"]>0.0) & (df["y coord"]<0.1428) & (df["Broken Bonds"]>0) ]
+    bb_df_2 = df[(df["y coord"]>0.1428) & (df["y coord"]<0.2857)& (df["Broken Bonds"]>0) ]
+    bb_df_3 = df[(df["y coord"]>0.2857) & (df["y coord"]<0.428) & (df["Broken Bonds"]>0) ]
+    bb_df_4 = df[(df["y coord"]>0.428) & (df["y coord"]<0.57) & (df["Broken Bonds"]>0) ]
+    bb_df_5 = df[(df["y coord"]>0.57) & (df["y coord"]<0.714) & (df["Broken Bonds"]>0) ]
+    bb_df_6 = df[(df["y coord"]>0.714) & (df["y coord"]<0.857) & (df["Broken Bonds"]>0) ]
+    bb_df_7 = df[(df["y coord"]>0.857) & (df["y coord"]<0.98) & (df["Broken Bonds"]>0) ]
 
-print(f'len 1  {len(bb_df_1)}, sum: {bb_df_1["Broken Bonds"].sum()}, ratio {ratio(bb_df_1)}')
-print(f'len 2  {len(bb_df_2)}, sum: {bb_df_2["Broken Bonds"].sum()}, ratio {bb_df_2["Broken Bonds"].sum()/len(bb_df_2)}')
-print(f'len 3  {len(bb_df_3)}, sum: {bb_df_3["Broken Bonds"].sum()}, ratio {bb_df_3["Broken Bonds"].sum()/len(bb_df_3)}')
-print(f'len 4  {len(bb_df_4)}, sum: {bb_df_4["Broken Bonds"].sum()}, ratio {bb_df_4["Broken Bonds"].sum()/len(bb_df_4)}')
-print(f'len 5  {len(bb_df_5)}, sum: {bb_df_5["Broken Bonds"].sum()}, ratio {bb_df_5["Broken Bonds"].sum()/len(bb_df_5)}')
-print(f'len 6  {len(bb_df_6)}, sum: {bb_df_6["Broken Bonds"].sum()}, ratio {bb_df_6["Broken Bonds"].sum()/len(bb_df_6)}')
-print(f'len 7  {len(bb_df_7)}, sum: {bb_df_7["Broken Bonds"].sum()}, ratio {bb_df_7["Broken Bonds"].sum()/len(bb_df_7)}')
+    bb_slow = len(bb_df_1) + len(bb_df_3) + len(bb_df_5) + len(bb_df_7)
+    bb_fast = len(bb_df_2) + len(bb_df_4) + len(bb_df_6)
+    
+    return bb_slow,bb_fast
 
-fig, ax1 = plt.subplots(nrows=1, ncols=1,figsize=(8,6))
-ax2 = ax1.twiny()
-ax3 = ax1.twiny()
 
-ax1.plot([ratio(bb_df_1),ratio(bb_df_2),ratio(bb_df_3),ratio(bb_df_4),ratio(bb_df_5),ratio(bb_df_6),ratio(bb_df_7)],[1,2,3,4,5,6,7],'o-',color='green')
-ax2.plot([bb_df_1["Broken Bonds"].sum(),bb_df_2["Broken Bonds"].sum(),bb_df_3["Broken Bonds"].sum(),bb_df_4["Broken Bonds"].sum(),bb_df_5["Broken Bonds"].sum(),bb_df_6["Broken Bonds"].sum(),bb_df_7["Broken Bonds"].sum()],[1,2,3,4,5,6,7],'o-',color='blue')
-ax3.plot([len(bb_df_1),len(bb_df_2),len(bb_df_3),len(bb_df_4),len(bb_df_5),len(bb_df_6),len(bb_df_7)],[1,2,3,4,5,6,7],'o-',color='orange')
-plt.show()
+def extract_numeric_value(directory_name):
+    return float(directory_name[3:])
+
+
+frac_list = []
+
+def_dirs = sorted((glob.glob("def*")), key=extract_numeric_value)   # used to be for "rt0.*" (relaxation threshold)
+print(f'def_dirs {def_dirs}')
+
+
+
+for def_dir in def_dirs:
+    os.chdir(def_dir)
+    x_variable = find_dirs()
+    for x in x_variable:
+        os.chdir(x)
+
+        melt_labels = find_dirs()
+        
+        for m in melt_labels:
+            os.chdir(m)
+            if os.path.isfile(filename):
+                slow_n,fast_n = get_bb_slow_and_fast(filename)
+                m_rate = float(getParameterFromLatte("input.txt","melt_increment"))
+                mu_f = float(getParameterFromLatte("input.txt","mu_f"))
+                def_rate = float(getParameterFromLatte("input.txt","defRate"))
+                frac_data = {
+                    'm_rate': m_rate,
+                    'mu_f': mu_f,
+                    'def_rate': def_rate,
+                    'slow_n': slow_n,
+                    'fast_n': fast_n
+                }
+                frac_list.append(frac_data)
+                print(frac_data)
+
+            os.chdir('..')
+        os.chdir('..')
+    os.chdir('..')
+    print("\n")
+
+
+df = pd.DataFrame(frac_list)
+
+print(df)
+# slow_n,fast_n = get_bb_slow_and_fast(filename)
+
+# print(f'slow_n {slow_n}, fast_n {fast_n}')
