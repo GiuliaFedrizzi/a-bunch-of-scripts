dfe2b194fdd449e4cc04a880e14c08e891938cfa
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-11-14 15:21:04 +0000

    use afmhot palette

--- post_processing/viz_a_file_porofix.py
+++ post_processing/viz_a_file_porofix.py
@@ -12,35 +12,36 @@ import glob
 import seaborn as sns
 from pathlib import Path
 import re   # regex
+import matplotlib as mpl
 
 from useful_functions import * 
 # from useful_functions_moments import *
 
 # options to set:
 plot_figure = 1
-dir_label = ''
 res = 200
-filename = "my_experiment00300.csv"
-first_part_of_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/singleInjection/porosity_fix/pf01'  # remember the slash (/) at the end
+filename = "my_experiment"
+filenum = "00000"
+first_part_of_path = '/nobackup/scgf/myExperiments/threeAreas/prod/prt/singleInjection/porosity_fix/'  
+pf = 'pf01'
+dir = first_part_of_path + pf
 
-#dir = first_part_of_path+'size'+str(dir_label)        # res200
-dir = first_part_of_path
 
-
-def read_calculate_plot(filename,scale_factor,res,var_plot):
+def read_calculate_plot(filenum,scale_factor,res,var_plot):
     """
     read the csv file, plot
     """
     variable_hue = var_plot 
 
-    myExp_all = pd.read_csv(filename, header=0)
+    myExp_all = pd.read_csv("my_experiment"+filenum+".csv", header=0)
     # myExp_all["x coord"] = myExp_all["x coord"]-0.0045
-    # myExp_all["y coord"] = myExp_all["y coord"]-0.006
+    # myExp_all["y coord"] = myExp_all["y coord"]+0.0005
     
 
-    myExpFluid_all = pd.read_csv("fluidgrid00300.csv", header=0)
+    myExpFluid_all = pd.read_csv("fluidgrid"+filenum+".csv", header=0)
     myExpFluid_all["x"] = myExpFluid_all["x"]/100
     myExpFluid_all["y"] = myExpFluid_all["y"]/100
+    print(f' {myExpFluid_all.head()}')
 
     myExp = myExp_all[(myExp_all['y coord'] >= 0.48) & 
         (myExp_all['y coord'] <= 0.52) &
@@ -56,15 +57,24 @@ def read_calculate_plot(filename,scale_factor,res,var_plot):
     print(f"myExpFluid max {max(myExpFluid['Porosity'])}, min {min(myExpFluid['Porosity'])}")
     if plot_figure:
         plt.figure()
-        ssize = 30 #scaled size for markers. Worked for plt.show 
-        # ssize = 4 #scaled size for markers
+        ssize = 50 #scaled size for markers. Worked for plt.show 
+        col_palette = "afmhot" #"gnuplot"
+
+        max_value = myExpFluid[variable_hue].max()#*1.1
+        min_value = myExpFluid[variable_hue].min()#*1.05
+        norm = mpl.colors.Normalize(vmin=min_value, vmax=max_value)
         
         scatter = sns.scatterplot(data=myExp,x="x coord",y="y coord",hue=variable_hue,
-            linewidth=0,alpha=0.8,marker="h",s=ssize,palette="cividis",legend=False).set_aspect('equal') #legend='full'
+            linewidth=0,alpha=0.8,marker="o",s=ssize,hue_norm=(min_value,max_value),palette=col_palette,legend=False).set_aspect('equal') #legend='full'
         
         scatter = sns.scatterplot(data=myExpFluid,x="x",y="y",hue=variable_hue,
-            linewidth=0,alpha=0.8,marker="s",s=ssize*2,palette="cividis",legend=False).set_aspect('equal') #legend='full'
-        
+            linewidth=0,alpha=0.8,marker="s",s=ssize*2,hue_norm=(min_value,max_value),palette=col_palette,legend=False).set_aspect('equal') #legend='full'
+        # Adding a color bar
+        norm = plt.Normalize(vmin=min_value, vmax=max_value)
+        sm = plt.cm.ScalarMappable(cmap=col_palette, norm=norm)
+        sm.set_array([])  # Only needed for the colorbar to display correctly
+        plt.colorbar(sm, label=variable_hue)  # Adding color bar with label
+
         for i in range(48,53):
             plt.axvline(x = i/100,color = 'k',alpha=0.5)
             plt.axhline(y = i/100,color = 'k',alpha=0.5)
@@ -78,7 +88,7 @@ def read_calculate_plot(filename,scale_factor,res,var_plot):
         plt.ylabel('y')
         
         # # plt.show()
-        # plt.savefig('stress_'+stress_type+'_'+ '{:.0e}'.format(threshold)+'.png',dpi=400)
+        # plt.savefig('porofix_'+pf+'_'+ 't'+filenum+'.png',dpi=400)
         # plt.clf()
     return variable_hue
 
@@ -89,23 +99,23 @@ input_tstep = float(getTimeStep(dir+"/input.txt"))
 # fig, axs = plt.subplots(nrows=nrow, ncols=ncol)#,constrained_layout=True)
 # all_axes=axs.reshape(-1) 
 
-timestep_number = int(re.findall(r'\d+',filename)[0])   # regex to find numbers in string. Then convert to float. Will be used to get "time" once multiplied by timestep.
+timestep_number = int(filenum)
+#= int(re.findall(r'\d+',filename)[0])   # regex to find numbers in string. Then convert to float. Will be used to get "time" once multiplied by timestep.
 
 print("\n",dir)
 """ loop through directories"""
 os.chdir(dir)
 
-# scale_factor = float(dir_label)  # factor for scaling the axes
 scale_factor = float(1)  # factor for scaling the axes
 
-myfile = Path(os.getcwd()+'/'+filename)  # build file name including path
+myfile = Path(os.getcwd()+'/my_experiment'+filenum+".csv")  # build file name including path
 if myfile.is_file():
-    variable_hue = read_calculate_plot(filename,scale_factor,res,"Porosity")
+    variable_hue = read_calculate_plot(filenum,scale_factor,res,"Porosity")
     
 
 #  some options for plotting
 if plot_figure:
-    plt.title("t = "+str('{:.1e}'.format(input_tstep*timestep_number))+", "+str(variable_hue)) 
+    # plt.title(pf+", t = "+str('{:.1e}'.format(input_tstep*timestep_number))+", "+str(variable_hue)) 
     #plt.savefig(fig_name, dpi=150)#,transparent=True)
     plt.show()
 
