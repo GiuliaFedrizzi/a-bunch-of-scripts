ab486cd989e8c39020ac051ad2689c2b35d06a84
GiuliaFedrizzi <scgf@leeds.ac.uk>
2024-11-18 15:59:37 +0000

    viz different geometries in one figure

---
+++ post_processing/viz_layers_geometries.py
@@ -0,0 +1,114 @@
+'''
+
+builds a table with images of porosity and broken bonds
+x axis: deformation
+y axis: melt rate contrast between layers
+
+Options:
+- can keep constant melt or constant strain: whats_constant  -   cm   or   cs
+- choose viscosity: visc_2_1e2   or   visc_3_1e3
+
+images are trimmed to show only the domain
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+
+from viz_functions import gallery,build_array,set_ax_options,find_variab,setup_array,find_dirs,setup_array_layers
+from useful_functions import getSaveFreq
+
+
+save_freq = int(getSaveFreq())
+
+times = list(range(110,120,10))
+# times = list(range(80,230,10))
+# times = list(range(40,400,10))
+# times = list(range(200,400,50))
+# translate to timestep
+times = [i*save_freq for i in times] 
+
+# viscosity_dirs = ["visc_2_1e2","visc_3_1e3"]
+visc = "visc_3_1e3"
+mr_contrast_dirs = ['lr43',                 'lr46',                    'lr47',                  'lr44',                        'lr48',                      'lr49']
+y_variable = ['lr43: Original Layers, mrc3','lr46: Thick Layers, mrc3','lr47: No central, mrc3','lr44: Original Layers,mrc1.5','lr48: Thick Layers, mrc1.5','lr49: No central, mrc1.5'] 
+# mr_contrast_dirs.reverse()
+print(f'len y_variable {len(y_variable)}')
+def_rate_dirs = ['def0e-8','def5e-7']
+melt_labels = '0.005'
+whats_constant = "cm"
+
+rose = False
+
+if rose == False:
+    im_length = 500
+    im_height = 506
+    ncols = len(def_rate_dirs)*2  # two, one for bb and one for porosity
+    for t in times:
+        array,def_rate_and_time = setup_array_layers(def_rate_dirs,mr_contrast_dirs,t,im_length,im_height,rose,save_freq,visc,whats_constant)   # load all files
+        result = gallery(array,ncols)           # organise in gallery view
+        fig, ax = plt.subplots()
+        ax.imshow(result.astype('uint8')) # uint8 explanation: https://stackoverflow.com/questions/49643907/clipping-input-data-to-the-valid-range-for-imshow-with-rgb-data-0-1-for-floa
+
+        ax.set_xticks(np.arange(im_length/2,im_length*len(def_rate_dirs)*2,im_length*2)) #  position and values of ticks on the x axis. Start: 437 (half width of an image) End: length of image times num of images in one row, Step: 883 (legth of image)
+        ax.set_xticklabels(def_rate_and_time,fontsize=7,rotation=20)
+
+        # y_variable = [str(i) for i in mr_contrast_dirs]   # convert to list of strings
+        ax.set_yticklabels(y_variable,fontsize=8)   #  position and values of ticks on the y axis. Start: 441 (half of image height) End: height of image times num of images in one column, Step: 883 (height of image)
+        y_ticks_positions = np.arange(im_height/2,im_height*len(y_variable)+im_height/2,im_height)
+        ax.set_yticks(y_ticks_positions)
+        if not os.path.exists('images_in_grid_layers'):
+            os.makedirs('images_in_grid_layers')
+        ax.set_title("$t_{ref}$ = "+str(t)+", visc "+visc,fontsize=12)
+
+        ax.set_ylabel('Melt Rate Contrast')
+        if visc == "visc_2_1e2":
+            filename = 'images_in_grid_layers/layer_geom_'+whats_constant+'_v1e2_t'
+        elif visc == "visc_3_1e3":
+            filename = 'images_in_grid_layers/layer_geom_'+whats_constant+'_v1e3_t'
+        else:
+            filename = 'images_in_grid_layers/layer_geom_'+whats_constant+'_t'
+
+        # plt.savefig(filename+str(t).zfill(3)+'.png',dpi=600)
+        
+        plt.show()
+
+        plt.clf()   # close figure
+
+if rose == True:
+    im_length = 1600
+    im_height = 1600
+    ncols = len(def_rate_dirs)
+    # time loop
+    for t in times:
+        array,def_rate_and_time = setup_array_layers(def_rate_dirs,mr_contrast_dirs,t,im_length,im_height,rose,save_freq,visc,whats_constant)   # load all files
+        result = gallery(array,ncols)           # organise in gallery view
+        fig, ax = plt.subplots()
+        ax.imshow(result.astype('uint8')) # uint8 explanation: https://stackoverflow.com/questions/49643907/clipping-input-data-to-the-valid-range-for-imshow-with-rgb-data-0-1-for-floa
+
+        ax.set_xticks(np.arange(im_length/2,im_length*len(def_rate_dirs),im_length)) #  position and values of ticks on the x axis. Start: 437 (half width of an image) End: length of image times num of images in one row, Step: 883 (legth of image)
+        ax.set_xticklabels(def_rate_and_time,fontsize=7,rotation=45)
+
+        # y_variable = [str(i) for i in mr_contrast_dirs]   # convert to list of strings
+        ax.set_yticklabels(y_variable,fontsize=10)   #  position and values of ticks on the y axis. Start: 441 (half of image height) End: height of image times num of images in one column, Step: 883 (height of image)
+        y_ticks_positions = np.arange(im_height/2,im_height*len(melt_labels)+im_height/2,im_height)
+        ax.set_yticks(y_ticks_positions)
+        if not os.path.exists('images_in_grid_layers'):
+            os.makedirs('images_in_grid_layers')
+        ax.set_title("$t_{ref}$ = "+str(t)+", visc "+visc,fontsize=12)
+
+        ax.set_ylabel('Melt Rate Contrast')
+        if visc == "visc_2_1e2":
+            filename = 'images_in_grid_layers/layer_rose_geom_'+whats_constant+'_v1e2_t'
+        elif visc == "visc_3_1e3":
+            filename = 'images_in_grid_layers/layer_rose_geom_'+whats_constant+'_v1e3_t'
+        else:
+            filename = 'images_in_grid_layers/layer_rose_geom_'+whats_constant+'_t'
+
+        # plt.savefig(filename+str(t).zfill(3)+'.png',dpi=600)
+        
+        plt.show()
+
+        plt.clf()   # close figure
+        plt.close()
