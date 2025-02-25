251ecba22ec5930cf37786518dff20c4ffd91802
GiuliaFedrizzi <scgf@leeds.ac.uk>
2025-02-25 16:33:05 +0000

    styling for figure

--- optimise/time_between_files.py
+++ optimise/time_between_files.py
@@ -3,9 +3,11 @@ import datetime
 import matplotlib.pyplot as plt
 from itertools import accumulate
 
+big_font = 15
+
 # Function to convert timestamp to datetime
 def get_creation_time(file_path):
-    timestamp = os.path.getctime(file_path)
+    timestamp = os.path.getmtime(file_path)  # mtime = last edit, not creation
     return datetime.datetime.fromtimestamp(timestamp)
 
 fig, ax = plt.subplots(2, 2, figsize=(9, 6))
@@ -17,7 +19,8 @@ ax3 = plt.subplot2grid((2, 2), (1, 1))
 
 # prt_dirs = ['depth1000','depth5000']
 # prt_dirs = ['threads1','threads2','threads4','threads8','threads16']#,'threads16']
-prt_dirs = ['op13/rt0.03/threads1','op14/rt0.03/threads1','op13/rt0.03/threads8','op14/rt0.03/threads8','op17/rt0.03/threads8','op18/rt0.03/threads8']#,'threads16']
+# prt_dirs = ['op13/rt0.03/threads1','op14/rt0.03/threads1','op13/rt0.03/threads8','op14/rt0.03/threads8','op17/rt0.03/threads8','op18/rt0.03/threads8']#,'threads16']
+prt_dirs = ['op13/rt0.03/threads1','op18/rt0.03/threads2','op18/rt0.03/threads4','op18/rt0.03/threads8','op18/rt0.03/threads16']#,'threads16']
 # second_part_of_path = 'scale1000/rb0.01'
 time_diffs_all = []
 
@@ -34,32 +37,39 @@ for i,prt_dir in enumerate(prt_dirs):
     # Calculate the time differences in minutes
     time_diffs = [(creation_times[i] - creation_times[i - 1]).total_seconds() / 60 for i in range(1, len(creation_times))]
     time_diffs = list(accumulate(time_diffs))
+
+    label = prt_dir.split("threads")[1] + " threads"
+    if prt_dir == 'op13/rt0.03/threads1':
+        time_diffs = time_diffs[:13]
+        label = "Serial"
+
+    print(f'{prt_dir}: time_diffs {len(time_diffs)}')
     time_diffs_all.append(time_diffs)
 
     # Plotting
-    ax1.plot(range(0,len(time_diffs)),time_diffs, marker='o',label=prt_dir)
+    ax1.plot(range(0,len(time_diffs)),time_diffs, marker='o',label=label)
 ax1.set_title("Time Elapsed Between Consecutive Files")
-ax1.set_xlabel("File Index")
+ax1.set_xlabel("File Index",fontsize=big_font)
 ax1.legend()
-ax1.set_ylabel("Time Elapsed (minutes), cumulative")
+ax1.set_ylabel("Time Elapsed (minutes), cumulative",fontsize=big_font)
 ax1.grid(True)
 
 # print(f'time_diffs_all {(time_diffs_all[0]))}')
 
-improvement = [(a - b) for a, b in zip(time_diffs_all[0], time_diffs_all[2])]  # this works even if one list is longer (extra elements are ignored)
+# improvement = [(a - b) for a, b in zip(time_diffs_all[0], time_diffs_all[2])]  # this works even if one list is longer (extra elements are ignored)
 
 
-ax2.plot(improvement, marker='o')
+# ax2.plot(improvement, marker='o')
 ax2.set_title("Time improvement")
 ax2.set_xlabel("File Index")
 ax2.set_ylabel("Improvement over time")
 ax2.grid(True)
 
 
-improvement = [(a - b)/a for a, b in zip(time_diffs_all[0], time_diffs_all[2])]  # this works even if one list is longer (extra elements are ignored)
+# improvement = [(a - b)/a for a, b in zip(time_diffs_all[0], time_diffs_all[2])]  # this works even if one list is longer (extra elements are ignored)
 
 
-ax3.plot(improvement, marker='o')
+# ax3.plot(improvement, marker='o')
 ax3.set_title("Time improvement ratio")
 ax3.set_xlabel("File Index")
 ax3.set_ylabel("Improvement/Original time")
