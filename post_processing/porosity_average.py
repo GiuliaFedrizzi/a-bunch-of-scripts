5ff47eb5ad0e33889ec1d53a00a5c0d80f30705a
GiuliaFedrizzi <scgf@leeds.ac.uk>
2025-02-27 15:09:01 +0000

    thick layers

--- post_processing/porosity_average.py
+++ post_processing/porosity_average.py
@@ -27,22 +27,24 @@ for i,filename in enumerate(sortedInputFiles[0:20:1]): #[beg:end:step]  set whic
         myExp = pd.read_csv(filename, header=0)
         # average everywhere
         average_porosity.append(myExp['Porosity'].mean()*100)
-        # average in the fast melting area only
-        fast1 = (myExp[(myExp["y coord"]>0.18) & (myExp["y coord"]<0.24)]["Porosity"])
-        fast2 = (myExp[(myExp["y coord"]>0.47) & (myExp["y coord"]<0.53)]["Porosity"])
-        fast3 = (myExp[(myExp["y coord"]>0.75) & (myExp["y coord"]<0.81)]["Porosity"])
+        # average in the fast melting area only    THICK LAYERS
+        fast1 = (myExp[(myExp["y coord"]>0.25) & (myExp["y coord"]<0.35)]["Porosity"])
+        fast2 = (myExp[(myExp["y coord"]>0.65) & (myExp["y coord"]<0.75)]["Porosity"])
+        # fast3 = (myExp[(myExp["y coord"]>0.75) & (myExp["y coord"]<0.81)]["Porosity"])
 
         # slow areas only:
-        slow1 = (myExp[(myExp["y coord"]>0.04) & (myExp["y coord"]<0.10)]["Porosity"])
-        slow2 = (myExp[(myExp["y coord"]>0.32) & (myExp["y coord"]<0.38)]["Porosity"])
-        slow3 = (myExp[(myExp["y coord"]>0.61) & (myExp["y coord"]<0.67)]["Porosity"])
-        slow4 = (myExp[(myExp["y coord"]>0.90) & (myExp["y coord"]<0.95)]["Porosity"])
+        slow1 = (myExp[(myExp["y coord"]>0.04) & (myExp["y coord"]<0.15)]["Porosity"])
+        slow2 = (myExp[(myExp["y coord"]>0.45) & (myExp["y coord"]<0.55)]["Porosity"])
+        slow3 = (myExp[(myExp["y coord"]>0.85) & (myExp["y coord"]<0.95)]["Porosity"])
+        # slow4 = (myExp[(myExp["y coord"]>0.90) & (myExp["y coord"]<0.95)]["Porosity"])
 
 
         # average_porosity_fast.append(myExp[(myExp["y coord"]>0.714) & (myExp["y coord"]<0.857)]["Porosity"].mean()*100)  # mean with conditions on x and y coordinates
-        average_porosity_fast.append(pd.concat([fast1, fast2, fast3]).mean()*100)  # mean with conditions on x and y coordinates
+        # average_porosity_fast.append(pd.concat([fast1, fast2, fast3]).mean()*100)  # mean with conditions on x and y coordinates
+        average_porosity_fast.append(pd.concat([fast1, fast2]).mean()*100)  # mean with conditions on x and y coordinates
         # average_porosity_slow.append(myExp[(myExp["y coord"]>0.857) & (myExp["y coord"]<1.0)]["Porosity"].mean()*100)  # mean with conditions on x and y coordinates
-        average_porosity_slow.append(pd.concat([slow1, slow2, slow3, slow4]).mean()*100)  # mean with conditions on x and y coordinates
+        average_porosity_slow.append(pd.concat([slow1, slow2, slow3]).mean()*100)  # mean with conditions on x and y coordinates
+        # average_porosity_slow.append(pd.concat([slow1, slow2, slow3, slow4]).mean()*100)  # mean with conditions on x and y coordinates
         file_num = float(filename.split("experiment")[1].split(".")[0])  # first take the part after "experiment", then the one before the "."
         time_array.append(file_num*input_tstep)
 
@@ -59,6 +61,7 @@ print("slow ",average_porosity_slow[-1]-average_porosity_slow[0],"%")
 
 s_in_year = 60*60*24*365
 print("rate = ",str(rate*s_in_year),"% per year")
+print("1% every ",str(1/(rate*s_in_year))," years")
 print("rate_fast = ",str(rate_fast*s_in_year),"% per year")
 print("rate_slow = ",str(rate_slow*s_in_year),"% per year")
 print(f'av rate from fast and slow {(rate_fast*3+rate_slow*4)/7*s_in_year}')
