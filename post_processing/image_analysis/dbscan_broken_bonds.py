339b8110a473fd6483077121f23d754b9a7aed2d
Giulia <scgf@leeds.ac.uk>
2025-07-01 12:41:10 +0100

    improve fig for paper. Don't save csv

--- post_processing/image_analysis/dbscan_broken_bonds.py
+++ post_processing/image_analysis/dbscan_broken_bonds.py
@@ -28,11 +28,12 @@ import pandas as pd
 import sys
 import os
 import matplotlib.gridspec as gridspec
-import statsmodels.api as sm
-from statsmodels.formula.api import ols
+from matplotlib.lines import Line2D
+# import statsmodels.api as sm
+# from statsmodels.formula.api import ols
 from scipy.stats import pearsonr
 
-sys.path.append('/home/home01/scgf/myscripts/post_processing')
+sys.path.append('/Users/giuliafedrizzi/Library/CloudStorage/OneDrive-UniversityofLeeds/PhD/scripts/a-bunch-of-scripts2/a-bunch-of-scripts/post_processing')
 
 from useful_functions import getSaveFreq,getParameterFromLatte
 from dbscan_functions import read_bb_data_from_csv,dbscan_and_plot,fill_db_with_NaN,draw_rose_ellipse
@@ -43,15 +44,28 @@ no_margins = 1
 
 # times = list(range(1, 20, 1)) + list(range(20, 141, 5)) + list(range(150, 501, 20)) + list(range(500, 801, 20)) + list(range(850, 1500, 40))
 # times = [88,100,300]
-times = [200]
+times = [100]
 times = [i*save_freq for i in times]   # convert to timestep
 
 
 plot_figures = True  # if I want to visualise clusters, ellipses etc
 
 
-
-
+plt.rcParams["font.weight"] = "bold"
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['axes.linewidth'] = 2
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['ytick.labelsize'] = 15
+        # plt.rcParams['ytick.labelsize'] = 15
+        # plt.rcParams['ytick.minor.size'] = 4
+        # plt.rcParams['ytick.minor.width'] = 2
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['xtick.major.width'] = 3
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['ytick.major.width'] = 3
+plt.rcParams['xtick.minor.size'] = 4
+plt.rcParams['xtick.minor.width'] = 2
 
 def plot_cluster_data(fig,df,cluster_variable,cluster_variable_label,no_margins,variab):
     # set which figure to use
@@ -61,16 +75,7 @@ def plot_cluster_data(fig,df,cluster_variable,cluster_variable_label,no_margins,
     # prepare to handle NaNs
     mask_nan = df[cluster_variable].isna()
 
-    # prepare to highlight big values
-    # smallest_value = df[cluster_variable].min()
-    # big_value = (df[cluster_variable].max() - smallest_value) * 3/4 + smallest_value
-    # print(f'{cluster_variable_label}: big_value {big_value}, max {df[cluster_variable].max()}, min {df[cluster_variable].min()}')
-    # df_big_values_mask = df[cluster_variable] > big_value
-
-
-    gs = gridspec.GridSpec(2, 2, width_ratios=[4, 4], height_ratios=[2, 7])
-    # Adjust layout to prevent overlap
-    # plt.tight_layout()
+    gs = gridspec.GridSpec(2, 2, width_ratios=[6,5], height_ratios=[3, 7])
     fig.suptitle(cluster_variable_label+", t = " + str(t))
     print(f'cluster_variable_label {cluster_variable_label}')
     ax0 = plt.subplot(gs[1, 0])
@@ -93,21 +98,47 @@ def plot_cluster_data(fig,df,cluster_variable,cluster_variable_label,no_margins,
     df['melt_rate'] = df['melt_rate'].astype(str)
 
     # pseudo heatmap
-    square_size = 2600
+    square_size = 3200
+    marker_scale_legend = 0.2  # how much smaller the item in the legend is
     # sns.heatmap(heatmap_data, ax=ax0, cmap="viridis", cbar_kws={'label': 'Average Size'})
     sns.scatterplot(data=df, x=variab,y='melt_rate',hue=cluster_variable,
                     marker='s',s=square_size,edgecolor='none', ax=ax0, palette="flare")
+    
+    legend_marker_size = square_size*marker_scale_legend*0.1
+    handles, labels = ax0.get_legend_handles_labels()
+
+    print(f'handles {handles}, labels {labels}')
+
+    # Add custom handle for "No clusters"
+    
+    no_clusters_handle = Line2D([0], [0], marker='s', color='w', label='No clusters',
+                                markerfacecolor='gray', markersize=legend_marker_size, linestyle='None')
+    handles.append(no_clusters_handle)
+    labels.append('No clusters')
+
+    print(f'handles {handles}, labels {labels}')
+
+    legend_title = cluster_variable_label
+    if cluster_variable_label == "Number of Clusters":
+        legend_title = "Number\nof Clusters"
+    elif cluster_variable_label == "Average Cluster Size":
+       legend_title = "Average\nCluster Size"
+    elif cluster_variable_label == "Average Deviation from 90 degrees":
+        legend_title = "Average\nDeviation from 90°"
+    ax0.legend(handles=handles, labels=labels, markerscale=marker_scale_legend, title=legend_title, fontsize=11, title_fontsize=12, loc='lower left', frameon=True)
+
+
     ax0.set_ylabel("Melt Rate")
 
     # Scatter plot for NaN values 
     sns.scatterplot(data=df[mask_nan], x=variab, y='melt_rate', color='gray',
-                    marker='s', s=square_size,edgecolor='none', ax=ax0)
+                    marker='s', s=square_size,edgecolor='none', ax=ax0, legend=False)
 
-    # # highlight big values
-    # sns.scatterplot(data=df[df_big_values_mask], x='viscosity', y='melt_rate',facecolors='none', hatch='///',
-    #                 marker='s',edgecolor='black', s=square_size*0.6, ax=ax0)
+    ax0.set_xlim([6e3, 5e6])  # set xlim to include an extra half square
 
+    plot_df = df[df[cluster_variable].notna()]
     df = df.sort_values(by=['melt_rate',variab])
+    plot_df = plot_df.sort_values(by=['melt_rate',variab])
 
 
     def create_palette(base_colors, n_colors):   # Function to create a color palette
@@ -115,33 +146,51 @@ def plot_cluster_data(fig,df,cluster_variable,cluster_variable_label,no_margins,
         # Generate colors from the colormap
         return [to_hex(colormap(i / (n_colors - 1))) for i in range(n_colors)]
 
-    palette_visc = create_palette(["#aae88e","#397367", "#140021"], len(df[variable_labels].unique()))
-    palette_visc_dict = dict(zip(df[variable_labels].unique(), palette_visc))
+    palette_visc = create_palette(["#aae88e","#397367", "#140021"], len(plot_df[variable_labels].unique()))
+    palette_visc_dict = dict(zip(plot_df[variable_labels].unique(), palette_visc))
 
-    palette_mr = create_palette(["#c2823a", "#33231E"], len(df['melt_rate'].unique()))
-    palette_mr_dict = dict(zip(df[variable_labels].unique(), palette_mr))
+    palette_mr = create_palette(["#c2823a", "#33231E"], len(plot_df['melt_rate'].unique()))
+    palette_mr_dict = dict(zip(plot_df[variable_labels].unique(), palette_mr))
 
     # Top plot (Viscosity vs Average Size)
     ax1 = plt.subplot(gs[0, 0], sharex=ax0)   # sharex  so they are aligned
-    sns.lineplot(data=df, x=variab, y=cluster_variable,hue="melt_rate", ax=ax1, palette=palette_mr)
-    sns.scatterplot(data=df, x=variab, y=cluster_variable,hue="melt_rate", ax=ax1, palette=palette_mr,legend='')
+    sns.lineplot(data=plot_df, x=variab, y=cluster_variable,hue="melt_rate", ax=ax1, palette=palette_mr)
+    sns.scatterplot(data=plot_df, x=variab, y=cluster_variable,hue="melt_rate", ax=ax1, palette=palette_mr,legend='')
     ax1.set_xlabel("")
     ax1.set_ylabel(cluster_variable_label)
+    if cluster_variable_label == "Number of Clusters":
+        ax1.set_ylabel("Number\nof Clusters")
+    elif cluster_variable_label == "Average Cluster Size":
+        ax1.set_ylabel("Average\nCluster Size")
+    elif cluster_variable_label == "Average Deviation from 90 degrees":
+        ax1.set_ylabel("Average\nDeviation from 90°")
+
     # ax1.legend(title="Melt Rate",ncol=1, fontsize=7, title_fontsize=9)
     ax1.xaxis.tick_top()
     # Invert the order in the legend (small at the bottom)
     handles, labels = ax1.get_legend_handles_labels()
-    ax1.legend(handles[::-1], labels[::-1],title="Melt Rate",fontsize=7)
-
+    ax1.legend(handles[::-1], labels[::-1],title="Melt Rate",fontsize=11, title_fontsize=12)
+    ax1.grid(linestyle='--',alpha=0.6)
 
     # Right plot (Melt Rate vs Average Size)
 
     ax2 = plt.subplot(gs[1, 1])#, sharey=ax0)    # sharey  so they are aligned
-    sns.lineplot(data=df, x="melt_rate", y=cluster_variable,hue=variable_labels, ax=ax2, palette=palette_visc_dict)
-    sns.scatterplot(data=df, x="melt_rate", y=cluster_variable,hue=variable_labels, ax=ax2, palette=palette_visc_dict,legend='')
+    sns.lineplot(data=plot_df, x="melt_rate", y=cluster_variable,hue=variable_labels, ax=ax2, palette=palette_visc_dict)
+    sns.scatterplot(data=plot_df, x="melt_rate", y=cluster_variable,hue=variable_labels, ax=ax2, palette=palette_visc_dict,legend='')
     ax2.set_xlabel("Melt Rate")
     # ax2.set_yticks("")  # no ticks - it removes them from the heatmap too
+    ax2.set_xlim([-1.65, 7.35])  # set xlim to include the original values, even if they are not in plot_df
+    plt.draw()
+    ticks = ax2.get_xticks()  # ticks are: [0, 1, 2, 3, 4, 5, 6, 7, 8]  -> they correspond to [0.002, 0.003, 0.004, 0.005, 0.006, 0.007, 0.008, 0.009]  !! note that the first tick (0) is 0.002 ! 
+    ticks = np.append(-1,ticks)
+    new_labels = ["" if i % 2 == 0 else (i+2)/1000 for i in ticks]  #  keep only every second label: [0.001, '', 0.003, '', 0.005, '', 0.007, '', 0.009]  i+2 because it starts counting from 0 and the first tick is 0.002
+    ax2.set_xticks(ticks)
+    ax2.set_xticklabels(new_labels)
+
     ax2.set_ylabel(cluster_variable_label)
+    if cluster_variable_label == "Average Deviation from 90 degrees":
+        ax2.set_ylabel("Average Deviation from 90°")
+    ax2.yaxis.set_label_position("right")
     # ax2.legend(title="$\mu_f$", fontsize=7, title_fontsize=9)
     ax2.yaxis.tick_right()
     # Invert the order in the legend (small at the bottom)
@@ -151,13 +200,16 @@ def plot_cluster_data(fig,df,cluster_variable,cluster_variable_label,no_margins,
     else:
         legend_title = "Def rate"
 
-    ax2.legend(handles[::-1], labels[::-1],title=legend_title,fontsize=7)
+    plt.subplots_adjust(wspace=0.03,hspace=0.03)  # adjust space between subplots
+
+    ax2.legend(handles[::-1], labels[::-1],title=legend_title,fontsize=11, title_fontsize=12)
+    ax2.grid(linestyle='--',alpha=0.6)
 
     # Show or save the plot
     # plt.show()
     
     if no_margins:  # excluding margins, use a different name
-        plt.savefig("cluster/cluster_x_"+cluster_variable+"_"+str(t)+".png")
+        plt.savefig("cluster/cluster_x1_"+cluster_variable+"_"+str(t)+".png")
     else:
         plt.savefig("cluster/cluster_"+cluster_variable+"_"+str(t)+".png")
 
@@ -207,10 +259,10 @@ def plot_pair_grid(df,t,no_margins):
     grid_mr.add_legend(title="Melt Rate")
     
     #   if excluding margins:
-    if no_margins:
-        plt.savefig("cluster/cluster_x_grid_mr"+str(t)+".png")
-    else:
-        plt.savefig("cluster/cluster_grid_mr"+str(t)+".png")
+    # if no_margins:
+    #     plt.savefig("cluster/cluster_x_grid_mr"+str(t)+".png")
+    # else:
+    #     plt.savefig("cluster/cluster_grid_mr"+str(t)+".png")
     
     plt.close(fig_pair)  # we're done with the figures
 
@@ -335,8 +387,8 @@ def cluster_analysis(t,no_margins):
             os.chdir('..')
 
             # now that it has read and calculated, save the data for next time
-        df.to_csv(csv_name)
-        print("saving")
+        # df.to_csv(csv_name)
+        # print("saving")
 
     else:
         df = pd.read_csv(csv_name,index_col=0)
@@ -347,7 +399,6 @@ def cluster_analysis(t,no_margins):
     if variab == "viscosity":
         df["viscosity"] = df["viscosity"]*1000
 
-    print(df)
 
     cluster_variables = {'cluster_n': "Number of Clusters",
             'average_size':"Average Cluster Size",
